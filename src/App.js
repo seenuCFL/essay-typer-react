677eb67bd23846d42e1272aff4c880e23d48f768
seenuCFL <seenuindia001@gmail.com>
2021-01-15 23:09:19 +0530

    added text-area functionality and basic css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import Search from './Search';
 import Display from './Display';
-import puppeteer from 'puppeteer';
+
+const text = `The purpose of this book is to give you a thorough introduction to competitive programming. It is assumed that you already know the basics of programming,
+but no previous background in competitive programming is needed.
+The book is especially intended for students who want to learn algorithms
+and possibly participate in the International Olympiad in Informatics (IOI) or in
+the International Collegiate Programming Contest (ICPC). Of course, the book is
+also suitable for anybody else interested in competitive programming.
+It takes a long time to become a good competitive programmer, but it is also
+an opportunity to learn a lot. You can be sure that you will get a good general
+understanding of algorithms if you spend time reading the book, solving problems
+and taking part in contests.
+The book is under continuous development. You can always send feedback on
+the book to ahslaaks@cs.helsinki.fi.`;
 
 class App extends Component{
   constructor(){
     super();
     this.state = {
-      searchKey: ''
+      searchKey: '',
+      currentPosition: 0,
+      word: ''
     }
   }
 
@@ -15,27 +29,26 @@ class App extends Component{
     this.setState({searchKey:event.target.value});
   }
 
-  wikiSearch = (event) =>{
-    const data = async function searchWikipedia(searchQuery) {
-        const endpoint = `https://en.wikipedia.org/wiki/${searchQuery}`;
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-        await page.goto(endpoint);
+  wikiSearch = () => {    
+    this.setState({currentPosition:0})
+  }
 
-        const [el] = await page.$x('//*[@id="mw-content-text"]/div[1]/p[2]');
-        const txt = el.getProperty('textContent');
-        const rawTxt = await txt.jsonValue();
-        console.log({rawTxt})
-      }
-    const p = data(this.state.searchKey);
+  printText = () => {
+    let x='';
+    let ran = Math.floor(Math.random()*5)+5;
+    for(let i=this.state.currentPosition; i<ran+this.state.currentPosition ;i++){
+      x+=text[i];
+    }
+    this.setState(Object.assign(this.state, {currentPosition: this.state.currentPosition+ran}))
+    this.setState(Object.assign(this.state, {word:this.state.word + x}))
   }
 
   render(){
     return(
       <React.Fragment>
-        <h1>Essay-Typer</h1>
-        <Search setInput={this.setInput} wikiSearch={this.wikiSearch}/>
-        <Display />
+        {/* <h1>Essay-Typer</h1>
+        <Search setInput={this.setInput} wikiSearch={this.wikiSearch}/><br></br> */}
+        <Display data={this.state.word} printText={this.printText} />
       </React.Fragment>
     );
   }
--- src/Display.js
+++ src/Display.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import './Display.css';
 
 class Display extends Component{
     render(){
         return(
             <React.Fragment>
-                <p>{this.props.data}</p>
+                <div className="heading">
+                    <h1>Panda</h1>
+                    <h2>Small article on panda</h2>
+                </div>
+                <div className="magic">
+                    <textarea id="ta" autoFocus readOnly onKeyPress={this.props.printText} value={this.props.data}></textarea>
+                </div>
             </React.Fragment>
         );
     }
