2637af436f41cb3d1bb778a03ca8a6f246f0b45c
seenuCFL <seenuindia001@gmail.com>
2021-01-16 15:44:30 +0530

    added css and scrapping basic

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import './Search.css';
 
 class Search extends Component{
     render(){
         return(
             <React.Fragment>
-                <input type='search' onChange={this.props.setInput}></input>
-                <button type='submit' onClick={this.props.wikiSearch}>submit</button>
+                <div class="support"></div>
+                <div className="container">
+                    <h1 id="title" className="flex-item">Essay-Typer</h1>
+                    <input id="input" className="flex-item" type='search' onChange={this.props.setInput}></input>
+                    <button className="flex-item" type='submit' onClick={this.props.wikiSearch}>submit</button>
+                </div>
             </React.Fragment>
         );
     }
